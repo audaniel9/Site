32f4edbb7db0e8b74ba0fde093dc84310c9205fe
Daniel A <audaniel9@gmail.com>
2019-02-07 14:26:49 -0500

    edit info

--- src/components/page_education.js
+++ src/components/page_education.js
@@ -8,8 +8,10 @@ class Education extends Component {
             <h3>Education</h3>
             <p class="lead">
                 Brooklyn College, City University of New York <br />
-                <i>B.S. in Computer Science</i> <br />
-                <i>Graduated December 2018</i> <br /> <br />
+                &nbsp; <i>Bachelor of Science</i> <br />
+                &nbsp; <i>Major: Computer Science</i> <br />
+                &nbsp; <i>Minor: Accounting</i> <br />
+                &nbsp; <i>Graduated December 2018</i> <br /> <br />
                 <b>Related Coursework: </b> Data Structures, Algorithms, Robotics, Design & Implementation Web Application, Computer Architecture, Object Oriented Programming,
                 Human Computer Interactions, Multimedia Computing, Operating Systems.
             </p>
