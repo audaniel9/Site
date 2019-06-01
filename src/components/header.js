e7afca83f1461739cc2ef111b80d286716a3ba92
Daniel A <audaniel9@gmail.com>
2019-06-01 19:38:52 -0400

    visual fixes

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div className="Header">
-        <header class="display-1">Daniel Au</header>
+      <div className="Header" class=" text-center">
+        <header class="display-1 pt-5">Daniel Au</header>
         <header class="display-4">Software Engineer</header>
-        <img class="selfpic" src={'public_assets/selfpic.jpg'} alt="selfpic"/>
+        <img class="selfpic m-5" src={'public_assets/selfpic.jpg'} alt="selfpic"/>
       </div>
     );
   }
